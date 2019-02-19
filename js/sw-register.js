bdc18936083896f5bcfa0f197f05d0e3d4b9f62b
StevenRJClarke1985 <srjc3@cantab.net>
2019-02-19 11:47:39 +0000

    FIX: addListener changed to addEventListener

--- js/sw-register.js
+++ js/sw-register.js
@@ -1,6 +1,6 @@
 // Taken from https:developers.google.com/web/fundamentals/primers/service-workers
 if ('serviceWorker' in navigator) {
-  window.addListener('load', () => {
+  window.addEventListener('load', () => {
     navigator.serviceWorker.register('./serviceWorker.js')
     .then( registration => console.log('ServiceWorker registration successful with scope: ', registration.scope) )
     .catch( error => console.log('ServiceWorker registration failed: ', error));
